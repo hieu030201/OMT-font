42664b94c7e703f886f8c8fff4eee01e06436d2f
Hieu <minhhieu6513@gmail.com>
2022-08-18 17:35:37 +0700

    first commit

---
+++ VnExpress/assets/js/main.js
@@ -0,0 +1,52 @@
+//    var lable = document.getElementsByClassName('collapsible-box');
+//     for(i=0;i<lable.length;i++){
+//         lable[i].addEventListener('click',function(){
+//             this.classList.toggle('active')
+//         })
+//     }
+
+// const collapse = document.getElementsByClassName("collapsible-btn")
+// for(let i=0; i< collapse.length;i++){
+//     collapse[i].addEventListener('click',function(){
+//         this.classList.toggle("activeColl")
+//         let content = this.nextElementSibling
+//         if(content.style.display === 'block'){
+//             content.style.display = ''
+//         }else{
+//             content.style.display = 'block'
+//         }
+//     })
+// }
+
+// window.addEventListener("scroll", function(){
+//     var container = document.getElementById("container");
+//     var nav = document.querySelector("nav");
+//     nav.classList.toggle("sticky", window.scrollY > container )
+// })
+
+// var container = document.getElementById("container");
+// var menu = document.getElementById("main-menu");
+//     if(window.pageYOffset >= 51){
+//         navbar.classList.add("sticky");
+//     }else{
+//         navbar.classList.remove("sticky");
+//     }
+// var menu = document.getElementById('nav');
+//     window.onscroll = function(){
+//         if(window.pageYOffset > 51){
+//             menu.style.position = "fixed";
+//             menu.style.top=0;
+//         }else{
+//             menu.style.position = "absolute";
+//             menu.style.top = 51;
+//         }
+// }
+
+// $(function(){
+//     $(window).scroll(function(){
+//        var scrolling = $(this).scrollTop();
+//        if(scrolling > 50){
+//         $(".menu-main").addClass
+//        }
+//     })
+// })
